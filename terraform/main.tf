7bb08d2213752a462fcb74d6b01e5faf72435d06
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2025-05-23 09:42:33 -0300

    style: format terraform files for improved readability

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,17 @@ module "hello_terraform" {
   role_arn      = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = var.lambda_runtime
-  
+
   filename         = "../dist/hello_terraform_lambda.zip"
   source_code_hash = filebase64sha256("../dist/hello_terraform_lambda.zip")
-  
+
   memory_size = 128
   timeout     = 10
-  
+
   environment_variables = {
     ENVIRONMENT = var.environment
   }
-  
+
   tags = {
     Environment = var.environment
     Function    = "hello-terraform"
@@ -31,17 +31,17 @@ module "add_item" {
   role_arn      = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = var.lambda_runtime
-  
+
   filename         = "../dist/add_item_lambda.zip"
   source_code_hash = filebase64sha256("../dist/add_item_lambda.zip")
-  
+
   memory_size = 256
   timeout     = 15
-  
+
   environment_variables = {
     DYNAMODB_TABLE_NAME = aws_dynamodb_table.market_list_table.name
   }
-  
+
   tags = {
     Environment = var.environment
     Function    = "add-item"
@@ -55,17 +55,17 @@ module "update_item" {
   role_arn      = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = var.lambda_runtime
-  
+
   filename         = "../dist/update_item_lambda.zip"
   source_code_hash = filebase64sha256("../dist/update_item_lambda.zip")
-  
+
   memory_size = 256
   timeout     = 15
-  
+
   environment_variables = {
     DYNAMODB_TABLE_NAME = aws_dynamodb_table.market_list_table.name
   }
-  
+
   tags = {
     Environment = var.environment
     Function    = "update-item"
@@ -79,17 +79,17 @@ module "delete_item" {
   role_arn      = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = var.lambda_runtime
-  
+
   filename         = "../dist/delete_item_lambda.zip"
   source_code_hash = filebase64sha256("../dist/delete_item_lambda.zip")
-  
+
   memory_size = 256
   timeout     = 15
-  
+
   environment_variables = {
     DYNAMODB_TABLE_NAME = aws_dynamodb_table.market_list_table.name
   }
-  
+
   tags = {
     Environment = var.environment
     Function    = "delete-item"
@@ -108,8 +108,8 @@ module "api_gateway" {
 
   api_name                        = "hello-terraform-api"
   environment                     = var.environment
-  cognito_user_pool_arn          = module.cognito.user_pool_arn
-  lambda_invoke_arn              = module.hello_terraform.invoke_arn
-  lambda_function_name           = module.hello_terraform.function_name
-  api_gateway_cloudwatch_role_arn = aws_iam_role.api_gateway_role.arn  
+  cognito_user_pool_arn           = module.cognito.user_pool_arn
+  lambda_invoke_arn               = module.hello_terraform.invoke_arn
+  lambda_function_name            = module.hello_terraform.function_name
+  api_gateway_cloudwatch_role_arn = aws_iam_role.api_gateway_role.arn
 }
