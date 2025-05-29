c651b48d541c93d8f1f66ddb8f10b9a81e62b6c3
Gabrielly Lorraynne <gabyalmeidacom@hotmail.com>
2025-05-29 10:02:49 -0400

    fix: Refatorando o Get

--- src/lambdas/get_item/lambda_function.py
+++ src/lambdas/get_item/lambda_function.py
@@ -11,15 +11,13 @@ def lambda_handler(event, context):
         query_params = event.get("queryStringParameters") or {}
         date = query_params.get("date")
 
-        
         pk = f"LIST#{date.replace('-', '')}"
         print(f"🔍 Buscando itens com PK: {pk}")
 
-        
         response = table.query(
              KeyConditionExpression=Key("PK").eq(pk)
         )
-       
+
         items = response.get("Items", [])
         print(f"📄 Itens encontrados: {json.dumps(items, indent=2)}")
 
